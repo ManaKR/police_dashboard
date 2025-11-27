ed6a044b2f864aaf56dd035c041f562efbd56d30
ManaKR <148516415+ManaKR@users.noreply.github.com>
2025-11-27 10:39:27 +0530

    Delete frontend/src directory

--- frontend/src/src/api.js
+++
@@ -1,6 +0,0 @@
-import axios from "axios";
-const API_URL = "http://localhost:5000";
-export const fetchVehicleData = async () => {
-  try { return (await axios.get(`${API_URL}/api/vehicles`)).data; }
-  catch (err) { console.error("Error fetching vehicles:", err); return {}; }
-};
